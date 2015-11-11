da38b344271ad64dbebc1826a056e43641260c60
misund <thomas@hemmeligadresse.com>
2015-11-11 21:02:34 +0100

    Fix reduce() error

--- src/weighted-random-object.js
+++ src/weighted-random-object.js
@@ -4,9 +4,7 @@
  * The objects should have a property with key 'weight' and a value that is a number.
  */
 module.exports = (objects) => {
-    let totalWeight = objects.reduce( (agg, object) => {
-        agg, object.weight;
-    }, 0);
+    let totalWeight = objects.reduce( (agg, object) => agg + object.weight, 0);
 
     let randomNumber = Math.random() * totalWeight;
 
